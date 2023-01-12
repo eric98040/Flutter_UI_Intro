5299a51c39db567aa1a4c07423ca7a07c7273a49
eric98040 <jwk0302@naver.com>
2023-01-13 06:45:20 +0900

    Update shoppingcart

---
+++ shoppingcart/lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFeeeeee); // App Brand Color
+const kSecondaryColor = Color(0xFFc6c6c6); // Basic Button Color
+const kAccentColor = Color(0xFFff7643); // Activation Button
